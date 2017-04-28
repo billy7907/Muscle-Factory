297df6187538a12ac2bb28ff47add3f2d47e0a21
Billy Hung <billy7907@gmail.com>
2017-04-28 11:28:14 -0700

    code clean up

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,18 +1,3 @@
 class ExercisesController < ApplicationController
 
-  before_action :find_exercise, only: :show
-
-  def index
-    @exercises = Exercise.all
-  end
-
-  def show
-  end
-
-
-  private
-
-  def find_exercise
-    @exercise = Exercise.find params[:id]
-  end
 end
